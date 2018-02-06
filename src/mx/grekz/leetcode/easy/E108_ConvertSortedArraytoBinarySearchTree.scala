26e056580c8b6d562986e513c919cf389df9355e
grekz <vidbla.grekz@gmail.com>
2018-02-06 04:51:20 +0100

    SLCE-108. Convert Sorted Array to Binary Search Tree

---
+++ src/mx/grekz/leetcode/easy/E108_ConvertSortedArraytoBinarySearchTree.scala
@@ -0,0 +1,19 @@
+package mx.grekz.leetcode.easy
+
+import mx.grekz.leetcode.helper.TreeNode
+
+object E108_ConvertSortedArraytoBinarySearchTree {
+  
+    def sortedArrayToBST(nums: Array[Int]): TreeNode = {
+        if ( nums.length == 0 ) { return null; }
+        def helper(l:Int, h:Int) : TreeNode = {
+            if ( l > h ) { return null; }
+            var m = l + ( h - l ) / 2;
+            var res = TreeNode(nums(m));
+            res.left = helper( l, m-1);
+            res.right = helper( m+1, h);
+            return res;            
+        }
+        return helper( 0, nums.length-1);
+    }
+}
